411d553c39464c85161d0c08715cd3a66d542a83
MancuQuelloBrutto <manuelebarone186@gmail.com>
2022-02-16 16:33:50 +0100

    Enter

--- script.js
+++ script.js
@@ -173,9 +173,19 @@ if(num == 3){
 let userAgent = navigator.userAgent;
 let browserName;
 
-if(userAgent.match(/safari/i)){
+if(userAgent.match(/chrome|chromium|crios/i)){
+    browserName = "chrome";
+  }else if(userAgent.match(/firefox|fxios/i)){
+    browserName = "firefox";
+  }  else if(userAgent.match(/safari/i)){
     browserName = "safari";
-}   
+  }else if(userAgent.match(/opr\//i)){
+    browserName = "opera";
+  } else if(userAgent.match(/edg/i)){
+    browserName = "edge";
+  }else{
+    browserName="No browser detection";
+  }
 
 document.querySelector("h1").innerText="You are using "+ browserName +" browser";
 //Check if Safari
